eed1d737f259c598d87ab2f6bae43885ef3fdcfe
Sujal <sujalkhatri411@gmail.com>
2024-10-11 20:38:37 -0400

    updated the route and controller for updateing a movie

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,43 +1,56 @@
 const Movie = require('../models/Movies');
-const fs =require('fs');
+const fs = require('fs');
 
-//Function to import all movies from json
-exports.importMovies = async(req,res)=>{
-    try{
+// Function to import all movies from json
+exports.importMovies = async (req, res) => {
+    try {
         const data = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'));
-        await Movies.insertMany(data);// import data from mongodb
-        res.status(200).send("movies imported sucessfully")
-    }
-    catch{(e)
+        await Movie.insertMany(data); // Use Movie instead of Movies
+        res.status(200).send("Movies imported successfully");
+    } catch (e) {
         console.error(e);
-    }
-
-    };
-    //Function to get all the files
-exports.getMovies = async(req,res)=>{
-    try{
-        const movies = await Movie.find();
-         res.status(200).json(movies);
-    }
-    catch(e){
-            console.error(e);
-            res.status(500).send('Error retrieving Movies');
+        res.status(500).send("Error importing movies"); // Send response on error
     }
 };
 
-//Function to create a new movie
-exports.createMovie = async(req,res)=>{
-    try{
+// Function to get all movies
+exports.getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.status(200).json(movies);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error retrieving movies');
+    }
+};
+
+// Function to create a new movie
+exports.createMovie = async (req, res) => {
+    try {
         const newMovie = new Movie(req.body);
         await newMovie.save();
-         res.status(201).json(newMovie);
-    }
-    catch(e){
-            console.error(e);
-            res.status(500).send('Error creating Movies');
+        res.status(201).json(newMovie);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error creating movie');
     }
 };
 
+// Get a single movie by Id
+exports.getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) {
+            return res.status(404).send('Movie not found');
+        }
+        res.status(200).json(movie);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error retrieving movie');
+    }
+};
+
+//update Movie
 
 //Get a single movie by Id
 exports.updateMovie = async(req,res) =>{
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,20 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/controllers');
 
-//Route to import movies
-router.post('/import',movieController.importMovies);
+// Route to import movies
+router.post('/import', movieController.importMovies);
 
-//Route to get all movies
-router.get('/',movieController.getMovies);
+// Route to get all movies
+router.get('/', movieController.getMovies);
 
-//Route to create a new movie
-router.post('/create',movieController.createMovie);
+// Route to create a new movie
+router.post('/create', movieController.createMovie);
+
+// Route to get a single movie by id
+router.get('/:id', movieController.getMovieById);
+
+//Route to update a movie by id
+
+router.put('/update/:id',movieController.updateMovie);
 
 module.exports = router;
-
-//Route to get a singlre movie by id
-router.get('/:id',movieController.getMovieById);
