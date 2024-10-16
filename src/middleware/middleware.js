fe14668df87afd770998f2aa5d8456c2b624ffc2
Sujal <sujalkhatri411@gmail.com>
2024-10-16 13:29:34 -0400

    function to create movie data before up and Creaate

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -3,3 +3,18 @@ const logger = (req,res,next)=>{
     console.log(`${req.method}${req.originalUrl} - ${new Date().toISOString()}`);
     next();
 }
+
+//Validate Movie data before creating or updating
+const validateMovie = (req,res,next)=>{
+
+    const{ title,studio,year,genres,directors} = req.body;
+    if(!title || !studio || !year || !genres || !directors){
+        return res.status(400).send('Missing required fields:title,studio,genres, year or directors');
+    }
+    if(typeof year !== 'number' || year < 1888 || year > new Date().getFullYear()){
+        return res.status(400).send('Invalid Year');
+    }
+
+    next();
+
+};
